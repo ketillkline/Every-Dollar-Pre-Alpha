ca42ce8a6391a96e6fdf6da06d6a59010811ddfd
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2025-12-23 09:52:46 -0500

    Created stronger password validation

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,1 +1,18 @@
-# include the class Complex Password Validator
+import re
+from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
+
+class StrongPasswordValidator:
+    def validate(self, password, user=None):
+
+        if len(password) < 8:
+            raise ValidationError (_("Error"))
+
+        if not re.search(r"[A-Z]", password):
+            raise ValidationError (_("Error"))
+
+        if not re.search(r"[a-z]", password):
+            raise ValidationError (_("Error"))
+
+        if not re.search(r"[!@#$%^&*()-+{}[]\|"':;><.,?/]', password):
+            raise ValidationError (_("Error"))
