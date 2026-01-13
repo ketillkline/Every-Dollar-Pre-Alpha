64b9ecaa0fc49970abb05958e2cac518ec482b2d
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-13 13:29:04 -0500

    Finished clear all button test and began work on an income submit button test; created a universal login function

--- e2e/test_home_page.py
+++ e2e/test_home_page.py
@@ -1,12 +1,19 @@
 from .conftest import page, test_user, url
 
-def test_clear_all_no_crash(page):
-
+def login(page):
     page.goto(url + "login/")
     page.fill("input[name='username']", "testuser")
     page.fill("input[name='password']", "testpassword")
     page.click("button[type='submit']")
 
+def test_clear_all(page):
+
+    login(page)
+
     clear_button = page.locator("button[value='clear_all_incomes']")
 
-    assert clear_button.is_visible()
+    page.wait_for_load_state("networkidle")
+    assert page.url == url
+
+def test_income_submit(page):
+    login(page)
