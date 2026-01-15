65cfe3138e17cbcb627730986c924799f1bb3e34
Joshua McGrath-Rodriguez <jrod4601@gmail.com>
2026-01-15 11:26:41 -0500

    added python-dateutil to requirements.txt

--- SimpleBudget/urls.py
+++ SimpleBudget/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from accounts import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.AuthViews.login_view, name='login'),
